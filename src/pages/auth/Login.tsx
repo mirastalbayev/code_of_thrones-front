bfc06eda91359232b8d0d37d5083f99633259e40
Miras_666 <52910217+mirastalbayev@users.noreply.github.com>
2023-04-08 16:04:03 +0600

    Незначительное изменение, поменял наз файлов

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,141 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import * as React from 'react';
+import {
+	Box,
+	Grid,
+	Checkbox,
+	Link,
+	Button,
+	CssBaseline,
+	TextField,
+	FormControl,
+	Typography,
+	Container,
+	InputLabel,
+	OutlinedInput,
+	InputAdornment,
+	IconButton,
+	FormControlLabel,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const LoginPage: NextPage = () => {
-	return <div>Login page</div>;
+	const [showPassword, setShowPassword] = React.useState(false);
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+	const handleMouseDownPassword = (
+		event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		event.preventDefault();
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Box sx={{ display: 'flex', alignItems: 'center' }}>
+						<Image src='/Logo.svg' alt='' width={50} height={52} />
+						<Typography component='h1' variant='h5' sx={{ ml: 2 }}>
+							INVOICE APP
+						</Typography>
+					</Box>
+					<Box
+						component='form'
+						onSubmit={handleSubmit}
+						noValidate
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email'
+							name='email'
+							autoComplete='email'
+							autoFocus
+						/>
+						<Grid item xs={12}>
+							<FormControl
+								sx={{ width: 396 }}
+								// Исправить размер sx 396
+								variant='outlined'
+							>
+								<InputLabel htmlFor='outlined-adornment-password'>
+									Пароль
+								</InputLabel>
+								<OutlinedInput
+									id='outlined-adornment-password'
+									type={showPassword ? 'text' : 'password'}
+									endAdornment={
+										<InputAdornment position='end'>
+											<IconButton
+												aria-label='toggle password visibility'
+												onClick={handleClickShowPassword}
+												onMouseDown={handleMouseDownPassword}
+												edge='end'
+											>
+												{showPassword ? <VisibilityOff /> : <Visibility />}
+											</IconButton>
+										</InputAdornment>
+									}
+									label='Password'
+									fullWidth
+									required
+								/>
+							</FormControl>
+						</Grid>
+						<FormControlLabel
+							control={<Checkbox value='remember' color='primary' />}
+							label='Запомнить меня'
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Авторизация
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href='#' variant='body2'>
+									Забыли пароль?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href='#' variant='body2'>
+									{'У вас нет аккаунта?'}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 };
 
 export default LoginPage;
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,7 +1,143 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import * as React from 'react';
+import {
+	Box,
+	Grid,
+	Link,
+	Button,
+	CssBaseline,
+	TextField,
+	FormControl,
+	Typography,
+	Container,
+	InputLabel,
+	OutlinedInput,
+	InputAdornment,
+	IconButton,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const RegisterPage: NextPage = () => {
-	return <div>Register page</div>;
+	const [showPassword, setShowPassword] = React.useState(false);
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+	const handleMouseDownPassword = (
+		event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		event.preventDefault();
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Box sx={{ display: 'flex', alignItems: 'center' }}>
+						<Image src='/Logo.svg' alt='' width={50} height={52} />
+						<Typography component='h1' variant='h5' sx={{ ml: 2 }}>
+							INVOICE APP
+						</Typography>
+					</Box>
+					<Box
+						component='form'
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									autoComplete='given-name'
+									name='firstName'
+									required
+									fullWidth
+									id='firstName'
+									label='Имя'
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id='email'
+									label='Email'
+									name='email'
+									autoComplete='email'
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<FormControl
+									sx={{ width: 396 }}
+									// Исправить размер sx 396
+									variant='outlined'
+								>
+									<InputLabel htmlFor='outlined-adornment-password'>
+										Пароль
+									</InputLabel>
+									<OutlinedInput
+										id='outlined-adornment-password'
+										type={showPassword ? 'text' : 'password'}
+										endAdornment={
+											<InputAdornment position='end'>
+												<IconButton
+													aria-label='toggle password visibility'
+													onClick={handleClickShowPassword}
+													onMouseDown={handleMouseDownPassword}
+													edge='end'
+												>
+													{showPassword ? <VisibilityOff /> : <Visibility />}
+												</IconButton>
+											</InputAdornment>
+										}
+										label='Password'
+										fullWidth
+										required
+									/>
+								</FormControl>
+							</Grid>
+						</Grid>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Регистрация
+						</Button>
+						<Grid container justifyContent='flex-end'>
+							<Grid item>
+								<Link href='#' variant='body2'>
+									У вас есть аккаунт?
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 };
 
 export default RegisterPage;
